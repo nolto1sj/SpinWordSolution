0f64a519677a70b2968e74fe919cd3f3a5319805
nolto1sj <sjnolton@gmail.com>
2022-10-30 23:48:48 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿static string SpinWord(string sentence)
+{
+    string[] wordArray = sentence.Split(' ');
+
+    for (int i = 0; i < wordArray.Length; i++)
+    {
+        if (wordArray[i].Length > 4)
+        {
+            char[] arr = wordArray[i].ToCharArray();
+            Array.Reverse(arr);
+            string word = new string(arr);
+            wordArray[i] = word;
+        }
+    }
+    return String.Join(" ", wordArray);
+}
